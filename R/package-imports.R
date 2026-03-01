015b743c00bbdc3e22b6d0da82b57bfcd11d42c1
Claudio Fronterre <cla.f90@gmail.com>
2026-03-01 19:29:42 +0000

    Add internal DAST PPC validation utilities and diagnostics
    - add ppc_dast_dgp and print.ppc_dast_dgp internal methods
    - add global, temporal, and IU posterior predictive summaries
    - add structured PPC plot outputs (global/temporal/iu)
    - add IU support diagnostics and misfit-focused IU plot
    - register S3 print method and update imports/namespace
    - refresh package documentation for new PPC functionality

--- R/package-imports.R
+++ R/package-imports.R
@@ -5,6 +5,11 @@
 ##' @importFrom Deriv Deriv
 ##' @importFrom numDeriv grad hessian
 ##' @importFrom ggplot2 ggplot aes geom_line geom_point theme labs
+##' @importFrom ggplot2 facet_wrap as_labeller
+##' @importFrom ggplot2 geom_histogram geom_vline
+##' @importFrom ggplot2 scale_x_continuous scale_color_manual scale_fill_manual scale_linetype_manual
+##' @importFrom ggplot2 scale_y_continuous
+##' @importFrom ggplot2 theme_bw
 ##' @importFrom gridExtra grid.arrange
 ##' @importFrom stats plogis
 ##' @importFrom dplyr mutate first
