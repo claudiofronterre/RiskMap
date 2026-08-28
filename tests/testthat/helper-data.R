3154b195d1421df3cacce8bc521a3d656bbac71a
simon-smart88 <simon.smart@cantab.net>
2026-08-28 14:14:43 +0100

    update helper-data

--- tests/testthat/helper-data.R
+++ tests/testthat/helper-data.R
@@ -2,11 +2,12 @@ set.seed(1)
 n <- 10
 coords <- cbind(runif(n, 0, 1), runif(n, 0, 1))
 
-data <- data.frame(lon = coords[,1],
-                 lat = coords[,2],
-                 cov = rnorm(n),
-                 den =  sample(5:20, n, replace = TRUE),
-                 i = 1:n)
+data <- data.frame(x = coords[,1],
+                   z = coords[,2],
+                   cov = rnorm(n),
+                   den =  sample(5:20, n, replace = TRUE),
+                   offset = rnorm(n),
+                   i = rep(1:(n/2), each = 2))
 
 sigma2 <- 1
 phi <- 2
@@ -17,10 +18,41 @@ Sigma_sroot <- t(chol(Sigma))
 S <- as.numeric(Sigma_sroot %*% rnorm(n))
 
 data$y <- 1 + 0.5 * data$cov + S + rnorm(n, sd = 0.1)
-sf_data <- sf::st_as_sf(data, coords = c("lon", "lat"), crs = 4326)
+gaussian_data <- st_as_sf(data, coords = c("x", "z"), crs = 32637)
 
-gaussian_model <- glgpm(y ~ cov + gp(),
-                         data = sf_data,
-                         family = "gaussian",
-                         scale_to_km = FALSE,
-                         messages = FALSE)
+# reduce iterations to speed up fits
+control_mcmc <- set_control_sim(n_sim = 1000, burnin = 200)
+
+gaussian_model <- glgpm(y ~ cov + gp() + re(i),
+                        data = gaussian_data,
+                        family = "gaussian",
+                        messages = FALSE)
+
+gaussian_offset_model <- glgpm(y ~ cov + gp(nugget = TRUE) + offset(offset),
+                               data = gaussian_data,
+                               family = "gaussian",
+                               fix_var_me = 0,
+                               messages = FALSE)
+
+eta <- 0.2 + 0.3 * data$cov + S
+p <- plogis(eta)
+data$y <- rbinom(n, size = data$den, prob = p)
+binomial_data <- st_as_sf(data, coords = c("x", "z"), crs = 32637)
+
+binomial_model <- glgpm(y ~ cov + gp() + re(i),
+                        data = binomial_data,
+                        family = "binomial",
+                        den = den,
+                        control_mcmc = control_mcmc,
+                        messages = FALSE)
+
+lambda <- exp(0.1 + 0.2 * data$cov + S)
+data$y <- rpois(n, lambda)
+poisson_data <- sf::st_as_sf(data, coords = c("x", "z"), crs = 32637)
+
+poisson_model <- glgpm(y ~ cov + gp() + re(i),
+                       data = poisson_data,
+                       family = "poisson",
+                       den = den,
+                       control_mcmc = control_mcmc,
+                       messages = FALSE)
